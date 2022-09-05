4f9c44b01ebce42ac01ed27b5321aaea97e47cb9
daren farina <daren.farina@gmail.com>
2022-09-05 12:23:56 -0400

    commit tree dff96607fbfb85ffb8fead85e76a900ae33c92ef

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
